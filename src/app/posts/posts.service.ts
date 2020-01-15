3a271c1731fbe24abb24d3695e2023bddc9761da
Mudit Mittal <mmudit30@gmail.com>
2020-01-15 17:43:10 +0530

    ..

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,7 +44,7 @@ export class PostsService{
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);    
         });
-    }
+    } 
 
     deletePost(postId: string){
         this.http.delete("http://localhost:3000/api/posts/" + postId)
